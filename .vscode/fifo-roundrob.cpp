cbe47d30d3969ba5bff684e013f7892fe110da35
dedarob <euclidesroberto16@gmail.com>
2025-02-27 18:29:15 -0300

    terminado roundrobin

--- .vscode/fifo-roundrob.cpp
+++ .vscode/fifo-roundrob.cpp
@@ -3,7 +3,7 @@
 
 void fazerFifo(std::map<int, int> &dadosMap)
 {
-    int tempoProcessTotal;
+    int tempoProcessTotal = 0;
     std::cout << "---começando fifo" << std::endl;
     for (auto i : dadosMap)
     {
@@ -26,17 +26,48 @@ bool checarCondicao(const std::map<int, int> &dadosMap)
             return true;
         }
     }
+    return false;
 }
 
 void fazerRobin(std::map<int, int> dadosMap)
 {
+    std::map<int, int> originalMap = dadosMap;
     std::cout << "---começando robin" << std::endl;
-    int tempoProcessTotal;
-    while ((checarCondicao))
+    int tempoProcessTotal = 0;
+
+    while (checarCondicao(dadosMap))
     {
-        // TODO: verificar se a solução usando o metodo checarCondicao não é O(n^2)
-        //  e tambem terminar a logica do robin
+        for (auto &i : dadosMap)
+        {
+            int valor = i.second;
+            if (valor <= 0)
+            {
+                continue;
+            }
+
+            std::cout << "processando " << i.first << "... tamanho " << i.second << std::endl;
+            if (valor <= 2)
+            {
+                i.second = 0;
+            }
+            else
+            {
+                i.second -= 2;
+            }
+
+            int porcent = ((originalMap.at(i.first) - i.second) * 100) / originalMap.at(i.first);
+            tempoProcessTotal += 4;
+
+            std::cout << "dado " << i.first << " está sendo processado e está em " << porcent << "%" << std::endl;
+
+            if (i.second == 0)
+            {
+                std::cout << "dado " << i.first << " foi processado" << std::endl;
+            }
+        }
     }
+    std::cout << "todos os dados concluidos com o tempo de " << tempoProcessTotal << std::endl;
+    std::cout << "---robin terminado--- \n\n\n";
 }
 
 int main()
@@ -47,4 +78,7 @@ int main()
     dadosMap[2] = 15;
     dadosMap[3] = 9;
     fazerFifo(dadosMap);
+    fazerRobin(dadosMap);
+
+    return 0;
 }
